b9e2f6681852c3fb83cf7675649bd91782950379
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2016-12-04 14:23:46 -0600

    modified script to get L4T 24.2.1 source

--- scripts/installKernelSources.sh
+++ scripts/installKernelSources.sh
@@ -3,8 +3,8 @@ apt-add-repository universe
 apt-get update
 apt-get install libqt4-dev pkg-config -y
 cd /usr/src
-wget http://developer.download.nvidia.com/embedded/L4T/r24_Release_v2.0/BSP/sources.tbz2
-tar -xvf sources.tbz2 source/kernel_src.tbz2
+wget http://developer.download.nvidia.com/embedded/L4T/r24_Release_v2.1/BSP/sources_r24.2.1.tbz2
+tar -xvf sources_r24.2.1.tbz2 source/kernel_src.tbz2
 tar -xvf source/kernel_src.tbz2
 cd kernel
 zcat /proc/config.gz > .config
