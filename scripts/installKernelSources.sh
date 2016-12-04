207affa6cb4fae204af1a9864a8e8ff2e6cbe61e
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2016-12-04 14:35:57 -0600

    fixed bug

--- scripts/installKernelSources.sh
+++ scripts/installKernelSources.sh
@@ -4,8 +4,8 @@ apt-get update
 apt-get install libqt4-dev pkg-config -y
 cd /usr/src
 wget http://developer.download.nvidia.com/embedded/L4T/r24_Release_v2.1/BSP/sources_r24.2.1.tbz2
-tar -xvf sources_r24.2.1.tbz2 source/kernel_src.tbz2
-tar -xvf source/kernel_src.tbz2
+tar -xvf sources_r24.2.1.tbz2 sources/kernel_src.tbz2
+tar -xvf sources/kernel_src.tbz2
 cd kernel
 zcat /proc/config.gz > .config
 make xconfig
